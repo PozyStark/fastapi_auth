ef84d02a9ae73224dda869ddf9a498a265b4861a
Павел Комаров <paul.jax@yandex.ru>
2023-12-14 00:15:28 +0500

    Расширение функционала репозитория

--- repositories/user.py
+++ repositories/user.py
@@ -1,29 +1,32 @@
-from sqlalchemy import select, union
+from sqlalchemy import select, union, union_all
 from models import GroupPermission, Permission, RolePermission, User, UserGroup, UserPermission, UserRole
 from databases import async_session
 from core.sqlalchemy_repository import SqlAlchemyRepository
 
 
 class UserRepository(SqlAlchemyRepository):
+
     model = User
 
-    async def find_user_permissions_by_id(user_id: str):
+    async def find_by_username(self, username: str) -> User | None:
+        result = await self.find_one_or_none(filters=[User.username==username])
+        return result
+    
+    async def find_user_permissions_by_id(self, user_id: str) -> list:
         async with async_session.begin() as session:
             user_permission_query = select(
-                UserPermission.perrmission_id,
-                Permission.id,
-                Permission.codename
+                Permission,
+                UserPermission.permission_id
             ).where(
                 UserPermission.user_id==user_id
             ).join(
                 Permission,
-                Permission.id==UserPermission.perrmission_id
+                Permission.id==UserPermission.permission_id
             )
 
             role_permission_query = select(
-                UserRole.role_id,
-                Permission.id,
-                Permission.codename
+                Permission,
+                UserRole.role_id
             ).where(
                 UserRole.user_id==user_id
             ).join(
@@ -31,13 +34,12 @@ class UserRepository(SqlAlchemyRepository):
                 RolePermission.role_id==UserRole.role_id
             ).join(
                 Permission,
-                Permission.id==RolePermission.perrmission_id
+                Permission.id==RolePermission.permission_id
             )
 
             group_permission_query = select(
-                UserGroup.group_id,
-                Permission.id,
-                Permission.codename,
+                Permission,
+                UserGroup.group_id
             ).where(
                 UserGroup.user_id==user_id
             ).join(
@@ -45,20 +47,16 @@ class UserRepository(SqlAlchemyRepository):
                 GroupPermission.group_id==UserGroup.group_id,
             ).join(
                 Permission,
-                Permission.id==GroupPermission.perrmission_id
+                Permission.id==GroupPermission.permission_id
             )
-            
-            permissions_union = union(
-                user_permission_query,
-                role_permission_query,
-                group_permission_query
-            ).order_by(Permission.id)
 
-            all_permissions_query = select(
-                Permission.id,
-                Permission.codename
-            ).from_statement(permissions_union)
+            user_permissions = await session.execute(user_permission_query)
+            role_permissions = await session.execute(role_permission_query)
+            group_permissions = await session.execute(group_permission_query)
 
-            permissions = await session.execute(all_permissions_query)
-            return [row for row in permissions.scalars()]
+            user_permissions_list = [row for row in user_permissions.scalars()]
+            role_permissions_list = [row for row in role_permissions.scalars()]
+            group_permissions_list = [row for row in group_permissions.scalars()]
+
+            return set(user_permissions_list+role_permissions_list+group_permissions_list)
 
